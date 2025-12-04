b22b4f9a287499947431de29553c1f36a60687dd
Dialexy <filiperamos212@gmail.com>
2025-12-04 15:03:55 +0000

    Need to come back to this, jumped to far into the deep-end

--- Seminar Tasks/Seminar 1/Exercise 4.2: Library Book Management System/src/Library.cpp
+++ Seminar Tasks/Seminar 1/Exercise 4.2: Library Book Management System/src/Library.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#inlcude <optional>
 
 
 class Library {
@@ -12,7 +13,7 @@ class Library {
         void addBook(const Book& book) {
             std::string isbn = book.getISBN();
 
-            if (books.contains(isbn)) {
+            if (books.find(isbn) != books.end()) {
                 std::cout << "This book is already in the library.\n";
             }
             else {
@@ -21,4 +22,14 @@ class Library {
             }
         }
 
-}
+        std::optional<Book> findBook(const std::string& isbn) const {
+            auto it = books.find(isbn);
+
+            if (it != books.end()) {
+                return it->second;
+            }
+
+            return std::nullopt;
+
+        }
+};
--- Seminar Tasks/Seminar 1/Exercise 4.2: Library Book Management System/src/Library.h
+++ Seminar Tasks/Seminar 1/Exercise 4.2: Library Book Management System/src/Library.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+#include <optional>
 #include "Book.h"
 
 class Library {
