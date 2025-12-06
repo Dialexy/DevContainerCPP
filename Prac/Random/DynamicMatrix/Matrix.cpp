7ac3d8f152c4fdc656c156826417786d7880a9c8
Dialexy <filiperamos212@gmail.com>
2025-12-06 17:59:56 +0000

    Matrix.cpp stared

--- Prac/Random/DynamicMatrix/Matrix.cpp
+++ Prac/Random/DynamicMatrix/Matrix.cpp
@@ -1,5 +1,55 @@
 #include <iostream>
+#include <vector>
 
 class Matrix {
 
+
+    private:
+        int r;
+        int c;
+        int** emptyArray;
+        int** array;
+
+
+    public:
+
+        Matrix() {
+            for (int i = 0; i < r; i++) {
+                emptyArray[i] = new int[c];
+            }
+
+            for (int i = 0; i < r; i++) {
+                for (int j = 0; j < c; j++) {
+                    emptyArray[i][j] = 0;
+                }
+            }
+        }
+
+        Matrix(int rows, int cols) {
+            r = rows;
+            c = cols;
+
+            for (int i = 0; i < rows; i++) {
+                array[i] = new int[cols];
+            }
+
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    array[i][j] = 0;
+                }
+            }
+        }
+
+        Matrix(Matrix &oldRows, Matrix &oldCols) const {
+
+        }
+
+        ~Matrix() {
+            for (int i = 0; i < r; i++) {
+                delete[] array[i];
+            }
+
+            delete[] array;
+        }
 };
+
