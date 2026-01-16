f0e18d4eb38500c79fd0d440ed8e0d08742bedca
Dialexy <filiperamos212@gmail.com>
2026-01-16 19:34:05 +0000

    reoved settings manager

--- Prac/OOP/Type-safe Settings Manager/Setting.cpp
+++
@@ -1,6 +0,0 @@
-#include "/Users/fil/code/Dev Container (C++)/Prac/OOP/Type-safe Settings Manager/Setting.h"
-
-//TODO: Look at Settings.h and complete each funcation according to the neede function.
-
-template <typename T> void set(const std::string &key, const T &value) {
-}
